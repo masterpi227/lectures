70c2636755e1c2e8ef32099f52455667b0c85953
masterpi227 <mazinemad@gmail.com>
2025-01-24 06:28:31 +0100

    Add files via upload

--- bi/infrastructure/bi-demo-stack/main.tf
+++ bi/infrastructure/bi-demo-stack/main.tf
@@ -44,6 +44,46 @@ resource "google_compute_instance" "instance-airflow" {
   }
 }
 
+resource "google_compute_instance" "instance-nifi" {
+  name                      = "instance-nifi"
+  zone                      = "us-central1-a"
+  tags                      = ["nifi"]
+  machine_type              = "n2-standard-4"
+  allow_stopping_for_update = true
+
+  boot_disk {
+    auto_delete = "true"
+
+    initialize_params {
+      image = "debian-cloud/debian-12"
+      size  = "50"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = <<SCRIPT
+    apt update
+    apt install -y git
+    git clone https://github.com/masterpi227/lectures.git
+    cd lectures/bi
+    bash docker.sh
+    bash nifi.sh
+    SCRIPT
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    
+    scopes = ["cloud-platform"]
+  }
+}
+
 resource "google_compute_instance" "instance-metabase" {
   name                      = "instance-metabase"
   zone                      = "us-central1-a"
@@ -134,6 +174,21 @@ resource "google_compute_firewall" "default-allow-airflow" {
   allow {
     protocol  = "tcp"
     ports     = ["8080", "3000"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["airflow"]
+}
+
+resource "google_compute_firewall" "default-allow-nifi" {
+  project     = var.project_id
+  name        = "default-allow-nifi"
+  network     = "default"
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["8080", "8443"]
   }
 
   source_ranges = ["0.0.0.0/0"]
